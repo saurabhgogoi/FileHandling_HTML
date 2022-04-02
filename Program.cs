4cc041606c5827cb94361d43c39429931494bec6
saurabhgogoi <101546679+saurabhgogoi@users.noreply.github.com>
2022-04-02 09:53:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HO_FileHandling_html
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File_Handle_Html fileHandling = new File_Handle_Html();
+            //fileHandling.WriteFile();
+            fileHandling.ReadFile();
+            Console.Read();
+        }
+    }
+}
