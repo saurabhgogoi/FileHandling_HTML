72d67e6aa278dc69e57483c1d62a0a68d3fd15e5
saurabhgogoi <101546679+saurabhgogoi@users.noreply.github.com>
2022-04-02 09:53:21 +0530

    Add files via upload

---
+++ File_Handle_Html.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HO_FileHandling_html
+{
+    class File_Handle_Html
+    {
+        public void ReadFile()
+        {
+            FileStream fileStream = new FileStream(@"C:\Users\Saurabh Gogoi\OneDrive\Desktop\mydata.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fileStream);
+            int count = 0;
+            string Word = "<input";
+            while (reader.Peek() > 0)
+            {
+                string line = reader.ReadLine();
+                if (line.Contains(Word))
+                {
+                    Console.WriteLine(line);
+                    count++;
+                }
+            }
+            Console.WriteLine("Number of <input = " + count);
+            reader.Close();
+            fileStream.Close();
+        }
+    }
+}
+
